c2daf121e324a6dce76fc26c27217325ba2579ac
Deeyaawudu <badiya61@gmail.com>
2023-09-07 12:07:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 /**
  * main	-	this is my main function
- * Return:	0
- * */
+ *
+ * Return:	0	(success)
+ */
+
 int	main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
