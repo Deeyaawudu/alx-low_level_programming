32291c478f89f4421b3809621a191cbd7ff88bfa
Deeyaawudu <badiya61@gmail.com>
2023-09-07 13:01:18 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 /**
- * main	-	printf	function
+ * main	-	a	printf	function
  * Return:	0
  */
 int	main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return	(0);
 }
 
 
