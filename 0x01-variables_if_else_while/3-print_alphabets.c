a2b4148f5f60acc93dc1ddd22a96969bf2399573
Deeyaawudu <badiya61@gmail.com>
2023-09-11 07:25:52 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,12 +7,15 @@
  */
 
 int	main(void)
-{char	letter;
-	for	(letter	=	'a';	letter	<=	'Z';	letter++)
-		putchar(letter);
 
-	for	(letter	=	'A';	letter	<=	'Z';	letter++)
+{
+	char	letter[52]	=	"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+		for(letter		=	'a';	letter	<=	'z';	letter++)
 		putchar(letter);
-	putchar('\n');
+	
+		for(letter		=	'A';	letter	<=	'Z';	letter++)
+		putchar(letter);
+		putchar('\n');
 	return	(0);
 }
