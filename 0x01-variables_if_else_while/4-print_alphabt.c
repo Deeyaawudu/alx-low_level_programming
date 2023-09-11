889fdb860c9cb39088203e09460103470c3c87e8
Deeyaawudu <badiya61@gmail.com>
2023-09-11 08:41:39 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include	<stdio.h>
 
 /**
- * main	-	Prints	the	alphabet	without	q	ande
+ * main	-	Prints	without	q	and	e
  *
  * Return:	Always	0
 */
@@ -12,7 +12,6 @@ int	main(void)
 		for	(i	=	97;	i	<	123;	i++)
 
 		putchar(i	!=	101	&&	i	!=	113);
-		
-	putchar('\n');
+		putchar('\n');
 	return	(0);
 }
